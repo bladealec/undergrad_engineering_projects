ba536a8558020132980d84e0c96a05efeadaeb41
Alec Blade <alecblade0@gmail.com>
2024-09-06 22:28:22 -0400

    works, but motors wont stop

--- rc_car.py
+++ rc_car.py
@@ -3,11 +3,12 @@ import RPi.GPIO as GPIO
 import numpy as np
 import decimal
 import os, sys
+import math
 
 #clears any previous gpio configurations
 GPIO.cleanup()
 
-#setup based on board pin numbers and not GPIO name
+#setup based on board pin numbers and noat GPIO name
 GPIO.setmode(GPIO.BOARD)
 
 #set up the input pins
@@ -29,9 +30,12 @@ PWM2 = GPIO.PWM(21,frequency) ;#left wheel
 
 #the activation of proportional voltage to
 #the pins. In this case it is 60% power
-PWM1.start(60)
-PWM2.start(60)
-
+pwm1_percent = 60
+pwm2_percent = 60
+PWM1.start(pwm1_percent)
+PWM2.start(pwm2_percent)
+power_l = pwm1_percent
+power_r = pwm2_percent
 
 #initiate variables
 l_count = 0
@@ -44,7 +48,12 @@ l_RPM = 0
 r_RPM = 0
 t = 0; #time
 t_prev = 0
-ARRAY_SIZE_ENCODER = frequency #averages data taken per second; increase for 'smoother' data
+
+#additional variables
+ARRAY_SIZE_ENCODER = 20  #data points for edge detection
+ARRAY_SIZE_RPM = 20       #data points for average RPM
+SAMPLE_TIME = 0.1  #time before RPM funct. calculastes speed; 10 samples per second
+
 
 #starting array
 l_array = np.full(ARRAY_SIZE_ENCODER, l_prev)
@@ -57,86 +66,101 @@ array_edge_high[:-edge_size] = 0
 array_edge_low = np.full(ARRAY_SIZE_ENCODER, 0)
 array_edge_low[:-edge_size] = 1
 
+
+
+
 def CounterFunction():
-	global l, r, l_count, l_array, r_array, array_edge_high, array_edge_low
+    global l, r, l_count, r_count, l_array, r_array, array_edge_high, array_edge_low
 
-l = GPIO.input(5)
-r = GPIO.input(3)
+    l = GPIO.input(5)
+    r = GPIO.input(3)
 
-#refresh the array - add the newest value to the front
-#and subtract the oldest value from the end
-l_array = np.insert(l_array, 0, l)
-l_array = np.delete(l_array, -1)
-r_array = np.insert(r_array, 0, r)
-r_array = npdelete(r_array, -1)
+    # refresh the array - add the newest value to the front
+    # and subtract the oldest value from the end
+    l_array = np.insert(l_array, 0, l)
+    l_array = np.delete(l_array, -1)
+    r_array = np.insert(r_array, 0, r)
+    r_array = np.delete(r_array, -1)
 
-#if there is an edge according to the edge array...
-if (r_array==array_edge_high).all() or (r_array==array_edge_low).all():
-	r_count += 1
-if (l_array==array_edge_high).all() or (l_array==array_edge_low).all():
-	l_count += 1
+    # if there is an edge according to the edge array...
+    if (r_array == array_edge_high).all() or (r_array == array_edge_low).all():
+        r_count += 1
+    if (l_array == array_edge_high).all() or (l_array == array_edge_low).all():
+        l_count += 1
+
+
 
 #RPM array
 l_RPM_array = np.full(ARRAY_SIZE_RPM, 0, dtype=decimal.Decimal)
 r_RPM_array = np.full(ARRAY_SIZE_RPM, 0, dtype=decimal.Decimal)
 
+#Initializing distance data collection
+wheel_diameter_inches = 2 #place holder value
+wheel_diameter_meters = wheel_diameter_inches * 0.0254
+circumference = math.pi * wheel_diameter_meters
+
+r_distance = 0
+l_distance = 0
+
+
+
 def RPM_function():
-	global l_count, r_count, l_count_prev, r_count_prev, l_RPM, r_RPM
-	global l_RPM_array, r_RPM_array, t, t_prev, l_distance, r_distance
+    global l_count, r_count, l_count_prev, r_count_prev, l_RPM, r_RPM
+    global l_RPM_array, r_RPM_array, t, t_prev, l_distance, r_distance
 	
-	#calculatin gow many counts have happened between the last implementation
-	l_RPM_now = (l_count - l_count_prev)/40*60/(t - t_prev)
-	r_RPM_now = (r_count - r_count_prev)/40*60/(t - t_prev)
+    #calculatin gow many counts have happened between the last implementation
+    l_RPM_now = (l_count - l_count_prev)/40*60/(t - t_prev)
+    r_RPM_now = (r_count - r_count_prev)/40*60/(t - t_prev)
 
-	#filter for RPM using arrays
-	l_RPM_array = np.insert(l_RPM_array, 0, l_RPM_now)
-	l_RPM_array = np.delete(l_RPM_array, -1)
-	r_RPM_array = np.insert(r_RPM_array, 0, r_RPM_now)
-	r_RPM_array = np.delete(r_RPM_array, -1)
+    #filter for RPM using arrays
+    l_RPM_array = np.insert(l_RPM_array, 0, l_RPM_now)
+    l_RPM_array = np.delete(l_RPM_array, -1)
+    r_RPM_array = np.insert(r_RPM_array, 0, r_RPM_now)
+    r_RPM_array = np.delete(r_RPM_array, -1)
 
-	#averaging the results of the arrays for a single value
-	l_RPM = np.mean(l_RPM_array)
-	r_RPM = np.mean(r_RPM_array)
+    #averaging the results of the arrays for a single value
+    l_RPM = np.mean(l_RPM_array)
+    r_RPM = np.mean(r_RPM_array)
 
-	#replacing the previous values
-	l_count_prev = l_count
-	r_count_prev = r_count
-	t_prev = t
+    #replacing the previous values
+    l_count_prev = l_count
+    r_count_prev = r_count
+    t_prev = t
+
+    #distance
+    time_elapsed = t - t_prev  # in seconds
+    speed_right = (r_RPM / 60) * circumference  # meters per second
+    speed_left = (l_RPM / 60) * circumference  # meters per second
+    
+    r_distance += speed_right * time_elapsed
+    l_distance += speed_left * time_elapsed
 
 t_start = time.time()
 t_sample = SAMPLE_TIME
-
-
-#seconds
 duration = 10
 
-with open("/home/pi/Desktop/encoder_data/name.csv", 'w') as f:
-	f.write(f'{text} ,,\n')
-	f.write(f'tiime,l_power,l_RPM,r_pwr,r_RPM\n')
-	PWM1.start(0)
-	PWM2.start(0)
+with open("/home/pi/Documents/encoder_data.csv", 'w') as f:
+    text = 'Encoder Data'
+    f.write(f'{text} ,,\n')
+    f.write(f'time,power_l,l_RPM,power_r,r_RPM\n')
+    PWM1.start(0)
+    PWM2.start(0)
 
-	t = 0 ; #time
-	t_prev = 0
-	t_start = time.time()
-	t_sample = SAMPLE_TIME
-	f.wite(f'{t},{l_RPM},{r_RPM}\n')
+    t = 0  # time
+    t_prev = 0
+    t_start = time.time()
+    t_sample = SAMPLE_TIME
+    f.write(f'{t},{l_RPM},{r_RPM}\n')
 
-	#run
-	while t < duration:
-		t = time.time() - t_start
-		CounterFunction()
+    # run
+    while t < duration:
+        t = time.time() - t_start
+        CounterFunction()
 
-		if t >= t_sample:
-			t_sample =+ SAMPLE_TIME
-			RPM_function()
-			PWM1.start(50)
-			PWM2.start(50)
-			print(f'time: {t:.02f} L power: {power_l:.02f} L RPM: {l_RPM:.02f} R pwr: {power_r:.02f} R RPM: {r_RPM:.02f}')
-			f.write(f'{t},{power_l},{l_distance}, {l_RPM},{power_r},{r_distance},{r_RPM}\n')
-
-
-
-
-
-			
+        if t >= t_sample:
+            t_sample += SAMPLE_TIME
+            RPM_function()
+            PWM1.start(50)
+            PWM2.start(50)
+            print(f'time: {t:.02f} L power: {power_l:.02f} L RPM: {l_RPM:.02f} R pwr: {power_r:.02f} R RPM: {r_RPM:.02f} L Sensor: {l:.02f} R Sensor: {r:.02f}')
+            f.write(f'{t},{power_l},{l_distance},{l_RPM},{power_r},{r_distance},{r_RPM},,{l},{r}\n')
